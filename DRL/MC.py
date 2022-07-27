351f3fae516eb193042b90d5007704ec154fb963
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:13:50 +0800

    Create MC.py

---
+++ DRL/MC.py
@@ -0,0 +1,19 @@
+# 对所有采样序列计算所有状态的价值
+def MC(episodes, V, N, gamma):
+    for episode in episodes:
+        G = 0
+        for i in range(len(episode) - 1, -1, -1):  #一个序列从后往前计算
+            (s, a, r, s_next) = episode[i]
+            G = r + gamma * G
+            N[s] = N[s] + 1
+            V[s] = V[s] + (G - V[s]) / N[s]
+
+
+timestep_max = 20
+# 采样1000次,可以自行修改
+episodes = sample(MDP, Pi_1, timestep_max, 1000)
+gamma = 0.5
+V = {"s1": 0, "s2": 0, "s3": 0, "s4": 0, "s5": 0}
+N = {"s1": 0, "s2": 0, "s3": 0, "s4": 0, "s5": 0}
+MC(episodes, V, N, gamma)
+print("使用蒙特卡洛方法计算MDP的状态价值为\n", V)
