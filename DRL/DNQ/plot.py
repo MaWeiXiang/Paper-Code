42baab4f855a34284a245226fe032735a81e1027
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:28:20 +0800

    Create plot.py

---
+++ DRL/DNQ/plot.py
@@ -0,0 +1,13 @@
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DQN on {}'.format(env_name))
+plt.show()
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DQN on {}'.format(env_name))
+plt.show()
