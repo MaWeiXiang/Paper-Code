23e650e00959c334117b673f4aac5bfa0b3603d4
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:04:10 +0800

    Create Solver.py
     Solver 基础类来实现上述的多臂老虎机的求解方案

---
+++ DRL/多臂老虎机/Solver.py
@@ -0,0 +1,25 @@
+class Solver:
+    """ 多臂老虎机算法基本框架 """
+    def __init__(self, bandit):
+        self.bandit = bandit
+        self.counts = np.zeros(self.bandit.K)  # 每根拉杆的尝试次数
+        self.regret = 0.  # 当前步的累积懊悔
+        self.actions = []  # 维护一个列表,记录每一步的动作
+        self.regrets = []  # 维护一个列表,记录每一步的累积懊悔
+
+    def update_regret(self, k):
+        # 计算累积懊悔并保存,k为本次动作选择的拉杆的编号
+        self.regret += self.bandit.best_prob - self.bandit.probs[k]
+        self.regrets.append(self.regret)
+
+    def run_one_step(self):
+        # 返回当前动作选择哪一根拉杆,由每个具体的策略实现
+        raise NotImplementedError
+
+    def run(self, num_steps):
+        # 运行一定次数,num_steps为总运行次数
+        for _ in range(num_steps):
+            k = self.run_one_step()
+            self.counts[k] += 1
+            self.actions.append(k)
+            self.update_regret(k)
