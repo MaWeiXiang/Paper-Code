404ffc4a77028345c00d70cc3767d67f64ebc1c3
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:25:43 +0800

    Update Dyna_Q.py

--- DRL/Dyna-Q/Dyna_Q.py
+++ DRL/Dyna-Q/Dyna_Q.py
@@ -109,3 +109,20 @@ def DynaQ_CliffWalking(n_planning):
                     })
                 pbar.update(1)
     return return_list
+
+np.random.seed(0)
+random.seed(0)
+n_planning_list = [0, 2, 20]
+for n_planning in n_planning_list:
+    print('Q-planning步数为：%d' % n_planning)
+    time.sleep(0.5)
+    return_list = DynaQ_CliffWalking(n_planning)
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list,
+             return_list,
+             label=str(n_planning) + ' planning steps')
+plt.legend()
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('Dyna-Q on {}'.format('Cliff Walking'))
+plt.show()
