be21082c1a8d367a120f683686c1357fe52bcb81
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:05:01 +0800

    Create EpsilonGreedy.py
    实现一个 epsilon -贪婪算法

---
+++ DRL/多臂老虎机/EpsilonGreedy.py
@@ -0,0 +1,17 @@
+class EpsilonGreedy(Solver):
+    """ epsilon贪婪算法,继承Solver类 """
+    def __init__(self, bandit, epsilon=0.01, init_prob=1.0):
+        super(EpsilonGreedy, self).__init__(bandit)
+        self.epsilon = epsilon
+        #初始化拉动所有拉杆的期望奖励估值
+        self.estimates = np.array([init_prob] * self.bandit.K)
+
+    def run_one_step(self):
+        if np.random.random() < self.epsilon:
+            k = np.random.randint(0, self.bandit.K)  # 随机选择一根拉杆
+        else:
+            k = np.argmax(self.estimates)  # 选择期望奖励估值最大的拉杆
+        r = self.bandit.step(k)  # 得到本次动作的奖励
+        self.estimates[k] += 1. / (self.counts[k] + 1) * (r -
+                                                          self.estimates[k])
+        return k
