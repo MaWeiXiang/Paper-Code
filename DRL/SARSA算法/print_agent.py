295730d67c150efd61dd3b59fc06367e3e756832
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:21:25 +0800

    Create print_agent.py

---
+++ DRL/SARSA算法/print_agent.py
@@ -0,0 +1,19 @@
+def print_agent(agent, env, action_meaning, disaster=[], end=[]):
+    for i in range(env.nrow):
+        for j in range(env.ncol):
+            if (i * env.ncol + j) in disaster:
+                print('****', end=' ')
+            elif (i * env.ncol + j) in end:
+                print('EEEE', end=' ')
+            else:
+                a = agent.best_action(i * env.ncol + j)
+                pi_str = ''
+                for k in range(len(action_meaning)):
+                    pi_str += action_meaning[k] if a[k] > 0 else 'o'
+                print(pi_str, end=' ')
+        print()
+
+
+action_meaning = ['^', 'v', '<', '>']
+print('Sarsa算法最终收敛得到的策略为：')
+print_agent(agent, env, action_meaning, list(range(37, 47)), [47])
