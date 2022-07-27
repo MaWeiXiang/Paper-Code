71a881fdf098caf6aff6d24d06ca87df4319509f
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:16:58 +0800

    Create ValueIteration.py

---
+++ DRL/Cliff-Walking/ValueIteration.py
@@ -0,0 +1,53 @@
+class ValueIteration:
+    """ 价值迭代算法 """
+    def __init__(self, env, theta, gamma):
+        self.env = env
+        self.v = [0] * self.env.ncol * self.env.nrow  # 初始化价值为0
+        self.theta = theta  # 价值收敛阈值
+        self.gamma = gamma
+        # 价值迭代结束后得到的策略
+        self.pi = [None for i in range(self.env.ncol * self.env.nrow)]
+
+    def value_iteration(self):
+        cnt = 0
+        while 1:
+            max_diff = 0
+            new_v = [0] * self.env.ncol * self.env.nrow
+            for s in range(self.env.ncol * self.env.nrow):
+                qsa_list = []  # 开始计算状态s下的所有Q(s,a)价值
+                for a in range(4):
+                    qsa = 0
+                    for res in self.env.P[s][a]:
+                        p, next_state, r, done = res
+                        qsa += p * (r + self.gamma * self.v[next_state] * (1 - done))
+                    qsa_list.append(qsa)  # 这一行和下一行代码是价值迭代和策略迭代的主要区别
+                new_v[s] = max(qsa_list)
+                max_diff = max(max_diff, abs(new_v[s] - self.v[s]))
+            self.v = new_v
+            if max_diff < self.theta: break  # 满足收敛条件,退出评估迭代
+            cnt += 1
+        print("价值迭代一共进行%d轮" % cnt)
+        self.get_policy()
+
+    def get_policy(self):  # 根据价值函数导出一个贪婪策略
+        for s in range(self.env.nrow * self.env.ncol):
+            qsa_list = []
+            for a in range(4):
+                qsa = 0
+                for res in self.env.P[s][a]:
+                    p, next_state, r, done = res
+                    qsa += p * (r + self.gamma * self.v[next_state] * (1 - done))
+                qsa_list.append(qsa)
+            maxq = max(qsa_list)
+            cntq = qsa_list.count(maxq)  # 计算有几个动作得到了最大的Q值
+            # 让这些动作均分概率
+            self.pi[s] = [1 / cntq if q == maxq else 0 for q in qsa_list]
+
+
+env = CliffWalkingEnv()
+action_meaning = ['^', 'v', '<', '>']
+theta = 0.001
+gamma = 0.9
+agent = ValueIteration(env, theta, gamma)
+agent.value_iteration()
+print_agent(agent, action_meaning, list(range(37, 47)), [47])
