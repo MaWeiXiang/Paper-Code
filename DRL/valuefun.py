7973c58b6fb61f5c2136500eef2c4c1dce2537d9
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:11:05 +0800

    Create valuefun.py

---
+++ DRL/valuefun.py
@@ -0,0 +1,10 @@
+def compute(P, rewards, gamma, states_num):
+    ''' 利用贝尔曼方程的矩阵形式计算解析解,states_num是MRP的状态数 '''
+    rewards = np.array(rewards).reshape((-1, 1))  #将rewards写成列向量形式
+    value = np.dot(np.linalg.inv(np.eye(states_num, states_num) - gamma * P),
+                   rewards)
+    return value
+
+
+V = compute(P, rewards, gamma, 6)
+print("MRP中每个状态价值分别为\n", V)
