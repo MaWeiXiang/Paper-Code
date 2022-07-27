87e4febe91fc592c5390cfc8bb35d3d7d6c42cbd
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:09:05 +0800

    Create ThompsonSampling.py
    汤普森采样算法

---
+++ DRL/多臂老虎机/ThompsonSampling.py
@@ -0,0 +1,22 @@
+class ThompsonSampling(Solver):
+    """ 汤普森采样算法,继承Solver类 """
+    def __init__(self, bandit):
+        super(ThompsonSampling, self).__init__(bandit)
+        self._a = np.ones(self.bandit.K)  # 列表,表示每根拉杆奖励为1的次数
+        self._b = np.ones(self.bandit.K)  # 列表,表示每根拉杆奖励为0的次数
+
+    def run_one_step(self):
+        samples = np.random.beta(self._a, self._b)  # 按照Beta分布采样一组奖励样本
+        k = np.argmax(samples)  # 选出采样奖励最大的拉杆
+        r = self.bandit.step(k)
+
+        self._a[k] += r  # 更新Beta分布的第一个参数
+        self._b[k] += (1 - r)  # 更新Beta分布的第二个参数
+        return k
+
+
+np.random.seed(1)
+thompson_sampling_solver = ThompsonSampling(bandit_10_arm)
+thompson_sampling_solver.run(5000)
+print('汤普森采样算法的累积懊悔为：', thompson_sampling_solver.regret)
+plot_results([thompson_sampling_solver], ["ThompsonSampling"])
