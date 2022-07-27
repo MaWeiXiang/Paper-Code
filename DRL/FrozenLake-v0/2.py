ea945876ff0ce62ff3bd74801f981b3d4a610442
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:18:33 +0800

    Create 2.py

---
+++ DRL/FrozenLake-v0/2.py
@@ -0,0 +1,6 @@
+action_meaning = ['<', 'v', '>', '^']
+theta = 1e-5
+gamma = 0.9
+agent = ValueIteration(env, theta, gamma)
+agent.value_iteration()
+print_agent(agent, action_meaning, [5, 7, 11, 12], [15])
