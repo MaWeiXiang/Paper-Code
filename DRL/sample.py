c9ede548d8cbaae9a9d7dd184517ae828b21d39d
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:13:31 +0800

    Create sample.py

---
+++ DRL/sample.py
@@ -0,0 +1,37 @@
+def sample(MDP, Pi, timestep_max, number):
+    ''' 采样函数,策略Pi,限制最长时间步timestep_max,总共采样序列数number '''
+    S, A, P, R, gamma = MDP
+    episodes = []
+    for _ in range(number):
+        episode = []
+        timestep = 0
+        s = S[np.random.randint(4)]  # 随机选择一个除s5以外的状态s作为起点
+        # 当前状态为终止状态或者时间步太长时,一次采样结束
+        while s != "s5" and timestep <= timestep_max:
+            timestep += 1
+            rand, temp = np.random.rand(), 0
+            # 在状态s下根据策略选择动作
+            for a_opt in A:
+                temp += Pi.get(join(s, a_opt), 0)
+                if temp > rand:
+                    a = a_opt
+                    r = R.get(join(s, a), 0)
+                    break
+            rand, temp = np.random.rand(), 0
+            # 根据状态转移概率得到下一个状态s_next
+            for s_opt in S:
+                temp += P.get(join(join(s, a), s_opt), 0)
+                if temp > rand:
+                    s_next = s_opt
+                    break
+            episode.append((s, a, r, s_next))  # 把（s,a,r,s_next）元组放入序列中
+            s = s_next  # s_next变成当前状态,开始接下来的循环
+        episodes.append(episode)
+    return episodes
+
+
+# 采样5次,每个序列最长不超过20步
+episodes = sample(MDP, Pi_1, 20, 5)
+print('第一条序列\n', episodes[0])
+print('第二条序列\n', episodes[1])
+print('第五条序列\n', episodes[4])
