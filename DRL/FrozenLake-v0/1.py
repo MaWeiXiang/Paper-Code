9631face0783b1d98d6696771f67f1e76e9ad85c
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-27 16:18:17 +0800

    Create 1.py

---
+++ DRL/FrozenLake-v0/1.py
@@ -0,0 +1,7 @@
+# 这个动作意义是Gym库针对冰湖环境事先规定好的
+action_meaning = ['<', 'v', '>', '^']
+theta = 1e-5
+gamma = 0.9
+agent = PolicyIteration(env, theta, gamma)
+agent.policy_iteration()
+print_agent(agent, action_meaning, [5, 7, 11, 12], [15])
