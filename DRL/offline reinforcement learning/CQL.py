d9bfb13df7a6f861ef9bdb944a29d7092f3bc592
那就开摆 <69625784+MaWeiXiang@users.noreply.github.com>
2022-07-28 08:55:10 +0800

    保守 Q-learning 算法
    保守 Q-learning（conservative Q-learning，CQL）:离线强化学习面对的巨大挑战是如何减少外推误差。实验证明，外推误差主要会导致在远离数据集的点上函数Q的过高估计，甚至常常出现Q值向上发散的情况。因此，如果能用某种方法将算法中偏离数据集的点上的函数Q保持在很低的值，或许能消除部分外推误差的影响

---
+++ DRL/offline reinforcement learning/CQL.py
@@ -0,0 +1,404 @@
+import numpy as np
+import gym
+from tqdm import tqdm
+import random
+import rl_utils
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+import matplotlib.pyplot as plt
+
+
+
+### 在倒立摆环境中从零开始训练一个在线 SAC 智能体，直到算法达到收敛效果，把训练过程中智能体采集的所有轨迹保存下来作为数据集
+class PolicyNetContinuous(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim, action_bound):
+        super(PolicyNetContinuous, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc_mu = torch.nn.Linear(hidden_dim, action_dim)
+        self.fc_std = torch.nn.Linear(hidden_dim, action_dim)
+        self.action_bound = action_bound
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        mu = self.fc_mu(x)
+        std = F.softplus(self.fc_std(x))
+        dist = Normal(mu, std)
+        normal_sample = dist.rsample()  # rsample()是重参数化采样
+        log_prob = dist.log_prob(normal_sample)
+        action = torch.tanh(normal_sample)
+        # 计算tanh_normal分布的对数概率密度
+        log_prob = log_prob - torch.log(1 - torch.tanh(action).pow(2) + 1e-7)
+        action = action * self.action_bound
+        return action, log_prob
+
+
+class QValueNetContinuous(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(QValueNetContinuous, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc_out = torch.nn.Linear(hidden_dim, 1)
+
+    def forward(self, x, a):
+        cat = torch.cat([x, a], dim=1)
+        x = F.relu(self.fc1(cat))
+        x = F.relu(self.fc2(x))
+        return self.fc_out(x)
+
+
+class SACContinuous:
+    ''' 处理连续动作的SAC算法 '''
+    def __init__(self, state_dim, hidden_dim, action_dim, action_bound,
+                 actor_lr, critic_lr, alpha_lr, target_entropy, tau, gamma,
+                 device):
+        self.actor = PolicyNetContinuous(state_dim, hidden_dim, action_dim,
+                                         action_bound).to(device)  # 策略网络
+        self.critic_1 = QValueNetContinuous(state_dim, hidden_dim,
+                                            action_dim).to(device)  # 第一个Q网络
+        self.critic_2 = QValueNetContinuous(state_dim, hidden_dim,
+                                            action_dim).to(device)  # 第二个Q网络
+        self.target_critic_1 = QValueNetContinuous(state_dim,
+                                                   hidden_dim, action_dim).to(
+                                                       device)  # 第一个目标Q网络
+        self.target_critic_2 = QValueNetContinuous(state_dim,
+                                                   hidden_dim, action_dim).to(
+                                                       device)  # 第二个目标Q网络
+        # 令目标Q网络的初始参数和Q网络一样
+        self.target_critic_1.load_state_dict(self.critic_1.state_dict())
+        self.target_critic_2.load_state_dict(self.critic_2.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
+                                                lr=actor_lr)
+        self.critic_1_optimizer = torch.optim.Adam(self.critic_1.parameters(),
+                                                   lr=critic_lr)
+        self.critic_2_optimizer = torch.optim.Adam(self.critic_2.parameters(),
+                                                   lr=critic_lr)
+        # 使用alpha的log值,可以使训练结果比较稳定
+        self.log_alpha = torch.tensor(np.log(0.01), dtype=torch.float)
+        self.log_alpha.requires_grad = True  #对alpha求梯度
+        self.log_alpha_optimizer = torch.optim.Adam([self.log_alpha],
+                                                    lr=alpha_lr)
+        self.target_entropy = target_entropy  # 目标熵的大小
+        self.gamma = gamma
+        self.tau = tau
+        self.device = device
+
+    def take_action(self, state):
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        action = self.actor(state)[0]
+        return [action.item()]
+
+    def calc_target(self, rewards, next_states, dones):  # 计算目标Q值
+        next_actions, log_prob = self.actor(next_states)
+        entropy = -log_prob
+        q1_value = self.target_critic_1(next_states, next_actions)
+        q2_value = self.target_critic_2(next_states, next_actions)
+        next_value = torch.min(q1_value,
+                               q2_value) + self.log_alpha.exp() * entropy
+        td_target = rewards + self.gamma * next_value * (1 - dones)
+        return td_target
+
+    def soft_update(self, net, target_net):
+        for param_target, param in zip(target_net.parameters(),
+                                       net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) +
+                                    param.data * self.tau)
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
+        rewards = (rewards + 8.0) / 8.0  # 对倒立摆环境的奖励进行重塑
+
+        # 更新两个Q网络
+        td_target = self.calc_target(rewards, next_states, dones)
+        critic_1_loss = torch.mean(
+            F.mse_loss(self.critic_1(states, actions), td_target.detach()))
+        critic_2_loss = torch.mean(
+            F.mse_loss(self.critic_2(states, actions), td_target.detach()))
+        self.critic_1_optimizer.zero_grad()
+        critic_1_loss.backward()
+        self.critic_1_optimizer.step()
+        self.critic_2_optimizer.zero_grad()
+        critic_2_loss.backward()
+        self.critic_2_optimizer.step()
+
+        # 更新策略网络
+        new_actions, log_prob = self.actor(states)
+        entropy = -log_prob
+        q1_value = self.critic_1(states, new_actions)
+        q2_value = self.critic_2(states, new_actions)
+        actor_loss = torch.mean(-self.log_alpha.exp() * entropy -
+                                torch.min(q1_value, q2_value))
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # 更新alpha值
+        alpha_loss = torch.mean(
+            (entropy - self.target_entropy).detach() * self.log_alpha.exp())
+        self.log_alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.log_alpha_optimizer.step()
+
+        self.soft_update(self.critic_1, self.target_critic_1)
+        self.soft_update(self.critic_2, self.target_critic_2)
+
+
+env_name = 'Pendulum-v0'
+env = gym.make(env_name)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+action_bound = env.action_space.high[0]  # 动作最大值
+random.seed(0)
+np.random.seed(0)
+env.seed(0)
+torch.manual_seed(0)
+
+actor_lr = 3e-4
+critic_lr = 3e-3
+alpha_lr = 3e-4
+num_episodes = 100
+hidden_dim = 128
+gamma = 0.99
+tau = 0.005  # 软更新参数
+buffer_size = 100000
+minimal_size = 1000
+batch_size = 64
+target_entropy = -env.action_space.shape[0]
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
+    "cpu")
+
+replay_buffer = rl_utils.ReplayBuffer(buffer_size)
+agent = SACContinuous(state_dim, hidden_dim, action_dim, action_bound,
+                      actor_lr, critic_lr, alpha_lr, target_entropy, tau,
+                      gamma, device)
+
+return_list = rl_utils.train_off_policy_agent(env, agent, num_episodes,
+                                              replay_buffer, minimal_size,
+                                              batch_size)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('SAC on {}'.format(env_name))
+plt.show()
+
+
+#### CQL
+class CQL:
+    ''' CQL算法 '''
+    def __init__(self, state_dim, hidden_dim, action_dim, action_bound,
+                 actor_lr, critic_lr, alpha_lr, target_entropy, tau, gamma,
+                 device, beta, num_random):
+        self.actor = PolicyNetContinuous(state_dim, hidden_dim, action_dim,
+                                         action_bound).to(device)
+        self.critic_1 = QValueNetContinuous(state_dim, hidden_dim,
+                                            action_dim).to(device)
+        self.critic_2 = QValueNetContinuous(state_dim, hidden_dim,
+                                            action_dim).to(device)
+        self.target_critic_1 = QValueNetContinuous(state_dim, hidden_dim,
+                                                   action_dim).to(device)
+        self.target_critic_2 = QValueNetContinuous(state_dim, hidden_dim,
+                                                   action_dim).to(device)
+        self.target_critic_1.load_state_dict(self.critic_1.state_dict())
+        self.target_critic_2.load_state_dict(self.critic_2.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
+                                                lr=actor_lr)
+        self.critic_1_optimizer = torch.optim.Adam(self.critic_1.parameters(),
+                                                   lr=critic_lr)
+        self.critic_2_optimizer = torch.optim.Adam(self.critic_2.parameters(),
+                                                   lr=critic_lr)
+        self.log_alpha = torch.tensor(np.log(0.01), dtype=torch.float)
+        self.log_alpha.requires_grad = True  #对alpha求梯度
+        self.log_alpha_optimizer = torch.optim.Adam([self.log_alpha],
+                                                    lr=alpha_lr)
+        self.target_entropy = target_entropy  # 目标熵的大小
+        self.gamma = gamma
+        self.tau = tau
+
+        self.beta = beta  # CQL损失函数中的系数
+        self.num_random = num_random  # CQL中的动作采样数
+
+    def take_action(self, state):
+        state = torch.tensor([state], dtype=torch.float).to(device)
+        action = self.actor(state)[0]
+        return [action.item()]
+
+    def soft_update(self, net, target_net):
+        for param_target, param in zip(target_net.parameters(),
+                                       net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) +
+                                    param.data * self.tau)
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(device)
+        actions = torch.tensor(transition_dict['actions'],
+                               dtype=torch.float).view(-1, 1).to(device)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(device)
+        rewards = (rewards + 8.0) / 8.0  # 对倒立摆环境的奖励进行重塑
+
+        next_actions, log_prob = self.actor(next_states)
+        entropy = -log_prob
+        q1_value = self.target_critic_1(next_states, next_actions)
+        q2_value = self.target_critic_2(next_states, next_actions)
+        next_value = torch.min(q1_value,
+                               q2_value) + self.log_alpha.exp() * entropy
+        td_target = rewards + self.gamma * next_value * (1 - dones)
+        critic_1_loss = torch.mean(
+            F.mse_loss(self.critic_1(states, actions), td_target.detach()))
+        critic_2_loss = torch.mean(
+            F.mse_loss(self.critic_2(states, actions), td_target.detach()))
+
+        # 以上与SAC相同,以下Q网络更新是CQL的额外部分
+        batch_size = states.shape[0]
+        random_unif_actions = torch.rand(
+            [batch_size * self.num_random, actions.shape[-1]],
+            dtype=torch.float).uniform_(-1, 1).to(device)
+        random_unif_log_pi = np.log(0.5**next_actions.shape[-1])
+        tmp_states = states.unsqueeze(1).repeat(1, self.num_random,
+                                                1).view(-1, states.shape[-1])
+        tmp_next_states = next_states.unsqueeze(1).repeat(
+            1, self.num_random, 1).view(-1, next_states.shape[-1])
+        random_curr_actions, random_curr_log_pi = self.actor(tmp_states)
+        random_next_actions, random_next_log_pi = self.actor(tmp_next_states)
+        q1_unif = self.critic_1(tmp_states, random_unif_actions).view(
+            -1, self.num_random, 1)
+        q2_unif = self.critic_2(tmp_states, random_unif_actions).view(
+            -1, self.num_random, 1)
+        q1_curr = self.critic_1(tmp_states, random_curr_actions).view(
+            -1, self.num_random, 1)
+        q2_curr = self.critic_2(tmp_states, random_curr_actions).view(
+            -1, self.num_random, 1)
+        q1_next = self.critic_1(tmp_states, random_next_actions).view(
+            -1, self.num_random, 1)
+        q2_next = self.critic_2(tmp_states, random_next_actions).view(
+            -1, self.num_random, 1)
+        q1_cat = torch.cat([
+            q1_unif - random_unif_log_pi,
+            q1_curr - random_curr_log_pi.detach().view(-1, self.num_random, 1),
+            q1_next - random_next_log_pi.detach().view(-1, self.num_random, 1)
+        ],
+                           dim=1)
+        q2_cat = torch.cat([
+            q2_unif - random_unif_log_pi,
+            q2_curr - random_curr_log_pi.detach().view(-1, self.num_random, 1),
+            q2_next - random_next_log_pi.detach().view(-1, self.num_random, 1)
+        ],
+                           dim=1)
+
+        qf1_loss_1 = torch.logsumexp(q1_cat, dim=1).mean()
+        qf2_loss_1 = torch.logsumexp(q2_cat, dim=1).mean()
+        qf1_loss_2 = self.critic_1(states, actions).mean()
+        qf2_loss_2 = self.critic_2(states, actions).mean()
+        qf1_loss = critic_1_loss + self.beta * (qf1_loss_1 - qf1_loss_2)
+        qf2_loss = critic_2_loss + self.beta * (qf2_loss_1 - qf2_loss_2)
+
+        self.critic_1_optimizer.zero_grad()
+        qf1_loss.backward(retain_graph=True)
+        self.critic_1_optimizer.step()
+        self.critic_2_optimizer.zero_grad()
+        qf2_loss.backward(retain_graph=True)
+        self.critic_2_optimizer.step()
+
+        # 更新策略网络
+        new_actions, log_prob = self.actor(states)
+        entropy = -log_prob
+        q1_value = self.critic_1(states, new_actions)
+        q2_value = self.critic_2(states, new_actions)
+        actor_loss = torch.mean(-self.log_alpha.exp() * entropy -
+                                torch.min(q1_value, q2_value))
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # 更新alpha值
+        alpha_loss = torch.mean(
+            (entropy - self.target_entropy).detach() * self.log_alpha.exp())
+        self.log_alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.log_alpha_optimizer.step()
+
+        self.soft_update(self.critic_1, self.target_critic_1)
+        self.soft_update(self.critic_2, self.target_critic_2)
+ 
+random.seed(0)
+np.random.seed(0)
+env.seed(0)
+torch.manual_seed(0)
+
+beta = 5.0
+num_random = 5
+num_epochs = 100
+num_trains_per_epoch = 500
+
+agent = CQL(state_dim, hidden_dim, action_dim, action_bound, actor_lr,
+            critic_lr, alpha_lr, target_entropy, tau, gamma, device, beta,
+            num_random)
+
+return_list = []
+for i in range(10):
+    with tqdm(total=int(num_epochs / 10), desc='Iteration %d' % i) as pbar:
+        for i_epoch in range(int(num_epochs / 10)):
+            # 此处与环境交互只是为了评估策略,最后作图用,不会用于训练
+            epoch_return = 0
+            state = env.reset()
+            done = False
+            while not done:
+                action = agent.take_action(state)
+                next_state, reward, done, _ = env.step(action)
+                state = next_state
+                epoch_return += reward
+            return_list.append(epoch_return)
+
+            for _ in range(num_trains_per_epoch):
+                b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                transition_dict = {
+                    'states': b_s,
+                    'actions': b_a,
+                    'next_states': b_ns,
+                    'rewards': b_r,
+                    'dones': b_d
+                }
+                agent.update(transition_dict)
+
+            if (i_epoch + 1) % 10 == 0:
+                pbar.set_postfix({
+                    'epoch':
+                    '%d' % (num_epochs / 10 * i + i_epoch + 1),
+                    'return':
+                    '%.3f' % np.mean(return_list[-10:])
+                })
+            pbar.update(1)
+            
+          
+        
+        
+epochs_list = list(range(len(return_list)))
+plt.plot(epochs_list, return_list)
+plt.xlabel('Epochs')
+plt.ylabel('Returns')
+plt.title('CQL on {}'.format(env_name))
+plt.show()
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('CQL on {}'.format(env_name))
+plt.show()
